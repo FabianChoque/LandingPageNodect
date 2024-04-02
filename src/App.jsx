260c54c85f42cd1905a1ac932b7c4fb3b43cfc21
Fabian Choque <fabianchoque28@gmail.com>
2024-04-02 01:52:48 -0300

    section contact

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
+import Contact from "./components/ui/Contact"
 import Navbar from "./components/ui/Navbar"
 import Presentation from "./components/ui/Presentation"
 
 function App() {
 
   return (
-    <div className="bg-bgbody h-screen px-2 xl:px-10">
+    <div className="px-2 bg-bgbody xl:px-10">
       <Navbar />
       <Presentation/>
+      <Contact />
     </div>
   )
 }
