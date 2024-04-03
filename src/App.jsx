10d557f76f4402c2d46c6b5708eb18f5f1842e96
Fabian Choque <fabianchoque28@gmail.com>
2024-04-02 22:18:01 -0300

    section partners and awards

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
+import Awards from "./components/ui/Awards"
 import Contact from "./components/ui/Contact"
 import Navbar from "./components/ui/Navbar"
+import Partners from "./components/ui/Partners"
 import Presentation from "./components/ui/Presentation"
 
 function App() {
@@ -8,6 +10,8 @@ function App() {
     <div className="px-2 bg-bgbody xl:px-10">
       <Navbar />
       <Presentation/>
+      <Partners />
+      <Awards />
       <Contact />
     </div>
   )
--- src/components/ui/Contact.jsx
+++ src/components/ui/Contact.jsx
@@ -12,19 +12,19 @@ const Contact = () => {
       }
 
   return (
-    <section className='min-h-[160vh] md:min-h-[50vh] xl:min-h-[100vh] pt-10'>
-        <div className='flex flex-col gap-5 text-center'>
-            <h1 className='text-4xl font-bold md:text-5xl text-primary'>Contáctanos</h1>
-            <p className='text-lg text-white'>¿Tienes alguna pregunta? Déjanos tu mensaje</p>
+    <section className="min-h-[160vh] md:min-h-[50vh] xl:min-h-[100vh] pt-10">
+        <div className="flex flex-col gap-5 text-center">
+            <h1 className="text-4xl font-bold md:text-5xl text-primary">Contáctanos</h1>
+            <p className="text-lg text-white">¿Tienes alguna pregunta? Déjanos tu mensaje</p>
         </div>
-        <div className='flex items-center justify-center pt-5'>
-            <div className='grid md:w-3/4 px-5 xl:px-0 grid-cols-1 md:grid-cols-4 xl:grid-cols-10 md:h-[50vh] xl:h-[70vh]'>
+        <div className="flex items-center justify-center pt-5">
+            <div className="grid md:w-3/4 px-5 xl:px-0 grid-cols-1 md:grid-cols-4 xl:grid-cols-10 md:h-[50vh] xl:h-[70vh]">
                 {/* Information */}
-                <div className='relative px-2 py-2 md:col-span-2 xl:col-span-4'>
-                    <div className='relative w-full h-full p-10 rounded-lg bg-primary'>
+                <div className="relative px-2 py-2 md:col-span-2 xl:col-span-4">
+                    <div className="relative w-full h-full p-10 rounded-lg bg-primary">
                         <div className="flex flex-col text-center xl:text-left">
-                            <h2 className='text-2xl font-bold text-white'>Información de contacto</h2>
-                            <p className='text-lg text-gray-300'>Canales directos de comunicación</p>
+                            <h2 className="text-2xl font-bold text-white">Información de contacto</h2>
+                            <p className="text-lg text-gray-300">Canales directos de comunicación</p>
                         </div>
                         <div className="flex flex-col gap-10 pt-10">
                             <div className="flex flex-col items-center gap-5 text-lg text-white xl:flex-row">
@@ -61,7 +61,7 @@ const Contact = () => {
 
                 </div>
                 {/* Contact Form */}
-                <form className='px-5 md:col-span-2 xl:col-span-6' onSubmit={handleSubmit(onSubmit)}>
+                <form className="px-5 md:col-span-2 xl:col-span-6" onSubmit={handleSubmit(onSubmit)}>
                     <div className="grid gap-8 mt-10 xl:grid-cols-2">
                         <div>
                             <label className="font-medium text-white text-md">Nombre</label>
