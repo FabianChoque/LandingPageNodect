cab4b577e63621a7a2bc97eab784d6902d8bc06c
Fabian Choque <fabianchoque28@gmail.com>
2024-04-19 11:38:08 -0300

    fix: refresh page in navigation responsive

--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -1,46 +1,84 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { disablePageScroll, enablePageScroll } from "scroll-lock";
 import { IoClose, IoMenu } from "react-icons/io5";
 
-const Navbar = () => {
+const navigation = [
+  {
+    title: "Bases",
+    url: "/bases",
+  },
+  {
+    title: "Empresa",
+    url: "/company",
+  },
+  {
+    title: "FAQ",
+    url: "/faqs",
+  },
+  {
+    title: "Contacto",
+    url: "/contacto",
+  },
+];
 
-  const [showMenu, setShowMenu] = useState(false);
+const Navbar = () => {
+  const [openNavigation, setOpenNavigation] = useState(false);
+
+  const toggleNavigation = () => {
+    if (openNavigation) {
+      setOpenNavigation(false);
+      enablePageScroll();
+    } else {
+      setOpenNavigation(true);
+      disablePageScroll();
+    }
+  };
+
+  const handleClick = () => {
+    if (!openNavigation) return;
+
+    enablePageScroll();
+    setOpenNavigation(false);
+  };
 
   return (
-    <header className="flex px-5 items-center justify-between xl:justify-start w-full h-[10vh] z-50">
-        <Link to="/" className="flex items-center w-1/2 gap-1 text-2xl font-bold text-primary">
-          <img
-            src="logo.png"
-            className="w-[40px] h-[40px] object-cover"
-          />
-          <p className="hidden xl:block">
-            Nodect
-          </p>
-        </Link>
-      <nav className={`fixed bg-bgbody w-[80%] xl:w-full h-full ${showMenu ? "left-0":"-left-full"}
-          top-0 xl:static flex flex-col xl:flex-row items-center justify-center xl:justify-end xl:p-5 gap-10 xl:gap-20 
-          transition-all duration-500 z-50`}>
-          <Link to="/bases" className="text-white">  
-            Bases
+    <header className="flex px-5 items-center justify-between w-full h-[10vh] z-50">
+      <Link
+        to="/"
+        className="flex items-center text-2xl font-bold text-primary"
+      >
+        <div className="flex items-center gap-1">
+          <img src="logo.png" className="w-[40px] h-[40px] object-cover" />
+          <p>Nodect</p>
+        </div>
+      </Link>
+      <nav
+        className={`${
+          // openNavigation ? "hidden" : "block"
+          openNavigation ? "flex" : "hidden"
+        } fixed text-xl bg-bgbody pt-20 top-[5rem] left-0 right-0 bottom-0 flex-col items-center gap-10
+        xl:static xl:flex xl:flex-row xl:pt-0 xl:gap-20 z-50`}
+      >
+        {navigation.map((item) => (
+          <Link
+            key={item.title}
+            to={item.url}
+            className="text-white transition-colors hover:text-primary"
+            onClick={handleClick}
+          >
+            {item.title}
           </Link>
-          <Link to="/company" className="text-white">  
-            Empresa
-          </Link>
-          <Link to="/faqs" className="text-white">  
-            FAQ 
-          </Link>
-          <Link to="/contacto" className="text-white">  
-            Contacto
-          </Link>
-        </nav>
-        <button 
-          onClick={() => setShowMenu(!showMenu)}
-          className="text-2xl xl:hidden text-primary"
-        >
-          {showMenu ? <IoClose /> : <IoMenu />} 
-        </button>
+        ))}
+      </nav>
+      <button
+        onClick={() => toggleNavigation()}
+        className="text-2xl xl:hidden text-primary"
+      >
+        {openNavigation ? <IoClose /> : <IoMenu />}
+      </button>
     </header>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
