4f5de7f1822dcbdeb2750b6c5450d1db82137094
Fabian Choque <fabianchoque28@gmail.com>
2024-04-19 20:33:54 -0300

    fix: add react router v6.22

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,14 @@
-import { Navigate, Route, Routes } from "react-router-dom"
+import { Outlet } from "react-router-dom"
+
 //Footer and header
 import Navbar from "./components/ui/Navbar"
 import Footer from "./components/ui/Footer"
 
-//Pages
-import Home from "./pages/Home";
-import Company from "./pages/Company";
-import Contact from "./components/ui/Contact"
-import Faqs from "./components/ui/Faqs"
-import Bases from "./components/ui/Bases";
-import Cookies from "./pages/Cookies";
-import Privacy from "./pages/PrivacyPolicy";
 function App() {
   return (
     <div className="px-2 bg-bgbody xl:px-10">
       <Navbar />
-      <Routes>
-        <Route path="/" element={ <Home />}/>
-        <Route path="/company" element={ <Company/>}/>
-        <Route path="/contacto" element={ <Contact />}/>
-        <Route path="/faqs" element={ <Faqs />}/>
-        <Route path="/bases" element={ <Bases /> } />
-        <Route path="/cookies" element={ <Cookies /> } />
-        <Route path="/privacy" element={ <Privacy /> } />
-        <Route path="/*" element={ <Navigate to='/'/>}/>
-      </Routes>
+      <Outlet />
       <Footer />
     </div>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,56 @@
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import { HashRouter } from "react-router-dom";
+import { Navigate, createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
+import Home from "./pages/Home";
+import App from "./App";
+import Company from "./pages/Company";
+import Contact from "./components/ui/Contact";
+import Faqs from "./components/ui/Faqs";
+import Bases from "./components/ui/Bases";
+import Cookies from "./pages/Cookies";
+import Privacy from "./pages/PrivacyPolicy";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/company",
+        element: <Company />,
+      },
+      {
+        path: "/contacto",
+        element: <Contact />,
+      },
+      {
+        path: "/faqs",
+        element: <Faqs />,
+      },
+      {
+        path: "/bases",
+        element: <Bases />,
+      },
+      {
+        path: "/cookies",
+        element: <Cookies />,
+      },
+      {
+        path: "/privacy",
+        element: <Privacy />,
+      },
+      {
+        path: "*",
+        element: <Navigate to="/company" replace />
+    }
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <HashRouter>
-    <App />
-  </HashRouter>
-)
+  <RouterProvider router={router} />
+);
