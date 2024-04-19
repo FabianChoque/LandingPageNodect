90b8954e06195be493a2e5a837bda306b38474a5
Fabian Choque <fabianchoque28@gmail.com>
2024-04-19 14:56:47 -0300

    doc: new pdf with bases

--- src/components/ui/Bases.jsx
+++ src/components/ui/Bases.jsx
@@ -6,12 +6,11 @@ function Bases() {
   return (
     <div className="flex flex-col items-center justify-center gap-8 xl:pt-10">
       <h1 className="px-5 text-4xl font-bold text-center md:text-5xl text-primary">Información sobre la competencia</h1>
-      <object
-        className="w-[90%] md:w-[80%] h-[100vh] "
-        data={bases}
-        type="application/pdf"
-
-      ></object>
+      <iframe
+        src="https://drive.google.com/file/d/1fQK6JhNDL-Lqfw16ARrATB8-NOv7_KdV/preview" 
+        className="w-[90%] md:w-[80%] h-[100vh]"
+        allow="autoplay"
+      ></iframe>
     </div>
   );
 }
